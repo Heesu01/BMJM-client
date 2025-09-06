458ecd097a566d4659c9ba11bc96f9e6f550f232
Heesu01 <rulru0413@naver.com>
2025-09-06 16:38:26 +0900

    hotfix: 배포 빌드 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -8,38 +8,50 @@ import fs from 'fs'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr({
-      svgrOptions: {
-        svgo: true,
-        svgoConfig: {
-          plugins: [
-            {
-              name: 'preset-default',
-              params: { overrides: { removeViewBox: false } },
-            },
-          ],
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development'
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      svgr({
+        svgrOptions: {
+          svgo: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'preset-default',
+                params: { overrides: { removeViewBox: false } },
+              },
+            ],
+          },
+          replaceAttrValues: {
+            '#000': 'currentColor',
+            '#000000': 'currentColor',
+          },
         },
-        replaceAttrValues: {
-          '#000': 'currentColor',
-          '#000000': 'currentColor',
-        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
     },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/localhost.pem')),
-    },
-    port: 3000,
-  },
+    server: isDev
+      ? {
+          https: {
+            key: fs.readFileSync(
+              path.resolve(__dirname, 'cert/localhost-key.pem'),
+            ),
+            cert: fs.readFileSync(
+              path.resolve(__dirname, 'cert/localhost.pem'),
+            ),
+          },
+          port: 3000,
+        }
+      : {
+          port: 3000,
+        },
+  }
 })
